4a2d244decead7246295a7b3764877abd3abb249
liaosi_work <liaosi_work@163.com>
2017-07-12 00:36:53 +0800

    # 利用socket进行TCP和UDP网络编程

---
+++ net-program/src/main/java/com/ddcx/exer/TCPexer.java
@@ -0,0 +1,20 @@
+package com.ddcx.exer;
+
+import org.junit.Test;
+
+/**
+ * Created by liaosi on 2017/7/12.
+ */
+//客户端给服务端发送文本，服务端会将文本转成大写再发给客户端
+public class TCPexer {
+
+    @Test
+    public void client() {
+
+    }
+
+    @Test
+    public void server() {
+
+    }
+}
