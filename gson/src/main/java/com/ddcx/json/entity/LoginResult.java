426bcdc7b88cbe817184c8745d060babbb7cb13f
liaosi <liaosilzu2007@163.com>
2017-10-28 22:48:32 +0800

    # gson 禁用html转义

---
+++ gson/src/main/java/com/ddcx/json/entity/LoginResult.java
@@ -0,0 +1,26 @@
+package com.ddcx.json.entity;
+
+/**
+ * Created by liaosi on 2017/10/28.
+ */
+public class LoginResult {
+
+    private long id;
+    private String token;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- gson/src/test/java/JsonTest.java
+++ gson/src/test/java/JsonTest.java
@@ -1,6 +1,8 @@
+import com.ddcx.json.entity.LoginResult;
 import com.ddcx.json.entity.Staff;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,11 +13,31 @@ import java.util.List;
  */
 public class JsonTest {
 
-    public static void main(String[] args) {
+
+    @Test
+    public void test2() {
+        LoginResult loginResult = new LoginResult();
+        loginResult.setId(983873L);
+        loginResult.setToken("uEzf/CX+QSBVWf4UDdTbWnX1d0/8G8iCjrZUBLt4t7cdzjx+kTApyxKeo9Y7TMLQPngpMNuoAH/kLZ9mamh6zs==");
+
+        Gson gson = new Gson();
+        System.out.println("测试1：使用默认的Gson对象调用toJson方法");
+        System.out.println(gson.toJson(loginResult));
+
+        System.out.println("========================================");
+
+        Gson gson1 = new GsonBuilder().disableHtmlEscaping().create();
+        System.out.println("测试1：使用禁用了html转椅的Gson对象调用toJson方法");
+        System.out.println(gson1.toJson(loginResult));
+    }
+
+
+    @Test
+    public void test() {
 
         Staff staff = createDummyObject();
 
-        //Gson gson = new Gson();
+//        Gson gson = new Gson();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         String json = gson.toJson(staff);
@@ -23,7 +45,7 @@ public class JsonTest {
 
     }
 
-    private static Staff createDummyObject() {
+    private Staff createDummyObject() {
 
         Staff staff = new Staff();
 
