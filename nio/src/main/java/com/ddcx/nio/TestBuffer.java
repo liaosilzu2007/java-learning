370220d0d48478eaeaf5745137c3921ce1bdbcbd
liaosi <liaosilzu2007@163.com>
2017-11-24 17:17:48 +0800

    #

---
+++ nio/src/main/java/com/ddcx/nio/TestBuffer.java
@@ -0,0 +1,103 @@
+package com.ddcx.nio;
+
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by liaosi on 2017/7/2.
+ */
+public class TestBuffer {
+
+    @Test
+    public void test1() {
+
+        String str = "abcde";
+
+        //1.分配一个指定大小的缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        System.out.println("---------allocate---------");
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        //2.使用put()方法存入数据到缓冲区
+        byteBuffer.put(str.getBytes());
+        System.out.println("---------put---------");
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        //3.flip()方法切换读取数据模式
+        byteBuffer.flip();
+        System.out.println("---------flip---------");
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        //4.利用get()方法读取缓冲区中的数据
+        byte[] bytes = new byte[byteBuffer.limit()];
+        byteBuffer.get(bytes);
+        System.out.println("---------get---------");
+        System.out.println(new String(bytes));
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        //5.利用rewind()方法可以再次重新读取
+        byteBuffer.rewind();
+        System.out.println("---------rewind---------");
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        //6.clear()方法，清空缓冲区，但是缓冲区中的数据依然存在，处于一个“被遗忘”的状态
+        byteBuffer.clear();
+        System.out.println("---------clear---------");
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println((char) byteBuffer.get());
+    }
+
+    @Test
+    public void test2() {
+        String str = "abcde";
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byteBuffer.put(str.getBytes());
+        byteBuffer.flip();
+        byte[] bytes = new byte[byteBuffer.limit()];
+
+        /*
+        get(byte[] dst, int offset, int length)方法：
+            从缓冲区读取length个元素，写入到byte数组索引为offset的位置。
+            如果缓冲区中的元素数量不够（读取时会超过limit值），或者byte数组的容量不够，都会报错。
+         */
+
+        byteBuffer.get(bytes, 0, 2);
+        System.out.println(new String(bytes));
+        System.out.println(byteBuffer.position());
+
+        //mark()方法：标记
+        byteBuffer.mark();
+        byteBuffer.get(bytes, 1, 3);
+        System.out.println(new String(bytes));
+        System.out.println(byteBuffer.position());
+
+        //reset()方法：恢复到mark的位置
+        byteBuffer.reset();
+        System.out.println(byteBuffer.position());
+    }
+
+    @Test
+    public void test3() {
+        //创建直接缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+
+        //isDrect()方法可以判断是否是直接缓冲区
+        System.out.println(byteBuffer.isDirect());
+
+    }
+}
---
+++ nio/src/main/java/com/ddcx/nio/TestChannal.java
@@ -0,0 +1,118 @@
+package com.ddcx.nio;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Created by liaosi on 2017/11/24.
+ */
+public class TestChannal {
+
+    //利用通道完成文件的复制（非直接缓冲区）
+    @Test
+    public void test1() {
+
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        FileChannel inChannel = null;
+        FileChannel outChannel = null;
+        try {
+            fis = new FileInputStream("C:/Users/liaosi/Desktop/1.jpg");
+            fos = new FileOutputStream("C:/Users/liaosi/Desktop/2.jpg");
+
+            //获取通道
+            inChannel = fis.getChannel();
+            outChannel = fos.getChannel();
+
+            //分配指定大小的缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            //将通道中的数据读取写到缓冲区中
+            while (inChannel.read(buffer) != 1) {
+                //切换缓冲区为读模式
+                buffer.flip();
+                //将缓冲区中的数据写入通道中
+                outChannel.write(buffer);
+                //清空缓冲区
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inChannel != null) {
+                    inChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (outChannel != null) {
+                    outChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //使用直接缓冲区完成文件的复制（内存映射文件，只有 ByteBuffer 支持内存映射文件）
+    @Test
+    public void test2() throws IOException {
+
+        FileChannel inChannel = FileChannel.open(Paths.get("C:/Users/liaosi/Desktop/1.jpg"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("C:/Users/liaosi/Desktop/3.jpg"), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+        //内存映射文件
+        MappedByteBuffer inMapBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+        MappedByteBuffer outMapBuffer = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, inChannel.size());
+
+        //直接对缓冲区进行数据的读写操作
+        byte[] bytes = new byte[inMapBuffer.limit()];
+        inMapBuffer.get(bytes);
+        outMapBuffer.put(bytes);
+
+        inChannel.close();
+        outChannel.close();
+    }
+
+    //通道之间直接传输数据（直接缓冲区）
+    @Test
+    public void test3() throws IOException {
+        FileChannel inChannel = FileChannel.open(Paths.get("C:/Users/liaosi/Desktop/1.jpg"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("C:/Users/liaosi/Desktop/4.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+        //数据从一个通道传输到另外一个通道
+        //inChannel.transferTo(0, inChannel.size(), outChannel);
+        outChannel.transferFrom(inChannel, 0, inChannel.size());
+
+        inChannel.close();
+        outChannel.close();
+
+    }
+
+}
