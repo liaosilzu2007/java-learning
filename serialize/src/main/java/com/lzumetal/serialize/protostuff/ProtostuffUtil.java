b92940dfc1653ee5cd0c4c7dba2610021ceda059
liaosi <liaosilzu2007@163.com>
2018-08-24 23:28:23 +0800

    # 序列化

--- serialize/src/main/java/com/lzumetal/serialize/protostuff/ProtostuffUtil.java
+++ serialize/src/main/java/com/lzumetal/serialize/protostuff/ProtostuffUtil.java
@@ -6,19 +6,34 @@ import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author liaosi
  * @date 2018-08-18
  */
 public class ProtostuffUtil {
 
+    private static Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
 
-    public static <T> byte[] serializer(T o) {
-        Class<T> clazz = (Class<T>) o.getClass();
-        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(cls);
+            cachedSchema.put(cls, schema);
+        }
+        return schema;
+    }
+
+
+    public static <T> byte[] serializer(T obj) {
+        Class<T> clazz = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);  //使用LinkedBuffer分配一块默认大小的buffer空间
         try {
-            Schema<T> schema = RuntimeSchema.getSchema(clazz);
-            return ProtobufIOUtil.toByteArray(o, schema, buffer);
+            //RuntimeSchema是一个包含业务对象所有信息的类，包括类信息、字段信息
+            Schema<T> schema = RuntimeSchema.getSchema(clazz);  //通过对象的类构建对应的schema
+            return ProtobufIOUtil.toByteArray(obj, schema, buffer);   //使用给定的schema将对象序列化为一个byte数组，并返回
         } catch (Exception e) {
             throw new IllegalStateException(e.getMessage(), e);
         } finally {
