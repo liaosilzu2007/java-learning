b19826b7e70aac9882edcf340631be7fccb0be40
liaosilzu2007 <liaosilzu2007@163.com>
2020-07-08 11:41:45 +0800

    # proxy

---
+++ proxy/src/main/java/com/lzumetal/java/learn/proxy/service/IService.java
@@ -0,0 +1,13 @@
+package com.lzumetal.java.learn.proxy.service;
+
+import java.sql.SQLException;
+
+/**
+ * @author liaosi
+ * @date 2020-07-08
+ */
+public interface IService {
+
+    void foo() throws SQLException;
+
+}
