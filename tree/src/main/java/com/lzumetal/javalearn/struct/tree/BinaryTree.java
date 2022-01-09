8f4667fe1c921b043b75e63203997b1ea47f398f
liaosilzu2007 <liaosilzu2007@163.com>
2022-01-09 17:28:25 +0800

    # 二叉树

---
+++ tree/src/main/java/com/lzumetal/javalearn/struct/tree/BinaryTree.java
@@ -0,0 +1,23 @@
+package com.lzumetal.javalearn.struct.tree;
+
+/**
+ * @author liaosi
+ * @date 2022-01-07
+ */
+public class BinaryTree {
+
+
+    /**
+     * 二叉树的二叉链表结点结构
+     */
+    class BiTNode<T> {
+
+        /* 节点数据 */
+        private T data;
+
+        /* 左右孩子指针 */
+        private BiTNode lchild, rchild;
+
+    }
+
+}
